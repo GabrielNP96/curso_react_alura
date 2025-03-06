2937f6f6420b0041c29dda516db046a7923f603b
Gabriel Nascimento <123754383+GabrielNP96@users.noreply.github.com>
2025-03-06 02:45:56 +0000

    Parâmetros de componente

--- organo/src/App.js
+++ organo/src/App.js
@@ -5,7 +5,10 @@ function App() {
   return (
     <div className="App">
       <Banner />
-      <CampoTexto />
+      <CampoTexto label="Nome" placeholder = "Digite seu nome"/>
+      <CampoTexto label="Cargo" placeholder = "digite seu cargo"/>
+      <CampoTexto label="Imagem" placeholder = "digite o endereço da imagem"/>
+
     </div>
   );
 }
