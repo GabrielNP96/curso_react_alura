03753242f7cf2b276fa95d07b2c580d92c50436d
Gabriel Nascimento <123754383+GabrielNP96@users.noreply.github.com>
2025-03-06 14:58:15 +0000

    usando map

---
+++ organo/src/components/ListaSuspensa/index.js
@@ -0,0 +1,13 @@
+const ListaSuspensa = (props) => {
+    console.log(props.itens)
+    return (
+        <div>
+            <label>{props.label}</label>
+                <select>
+                    {props.itens.map(item => <option>{item}</option>)}
+                </select>
+        </div>
+    )
+}
+
+export default ListaSuspensa
